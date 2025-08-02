447e5f2a1633d19560477687d5eed8f8532b0f8b
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2025-08-02 17:26:45 +0300

    Create leaderboard.js

---
+++ api/leaderboard.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../data.json'), 'utf-8'));
+  res.status(200).json(data.leaderboard);
+};
