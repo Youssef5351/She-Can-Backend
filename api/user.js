9436fdcb6a35f0071ae9317f6a035d2391eb25e4
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2025-08-02 17:38:04 +0300

    Update user.js

--- api/user.js
+++ api/user.js
@@ -2,11 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  try {
-    const filePath = path.join(__dirname, 'data.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-    res.status(200).json(data.user);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to read user data', details: error.message });
-  }
+  const filePath = path.join(__dirname, 'data.json');
+  const jsonData = fs.readFileSync(filePath, 'utf-8');
+  const data = JSON.parse(jsonData);
+  res.status(200).json(data.user);
 };
